158f0ae56e8e7d5e170d1afd3e7a574f68c63c3a
João Pedro A. Hypólito <jpazhy@gmail.com>
2024-02-02 12:15:11 -0300

    feat-cria-main
    Cria a java main

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "João Pedro";
+
+        System.out.println("Olá, " + nome + "!");
+        System.out.println("Tudo bem?");
+    }
+}
