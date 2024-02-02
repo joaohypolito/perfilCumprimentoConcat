6ab0ec384c49f2300cc499c0f9d6235d30948107
João Pedro A. Hypólito <jpazhy@gmail.com>
2024-02-02 12:22:45 -0300

    feat-cria-class-Perfil
    Altera a Main e cria a classe Perfil

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,3 @@
 public class Main {
-    public static void main(String[] args) {
-        String nome = "João Pedro";
 
-        System.out.println("Olá, " + nome + "!");
-        System.out.println("Tudo bem?");
-    }
 }
---
+++ src/Perfil.java
@@ -0,0 +1,16 @@
+/*
+- Crie um projeto Java no IntelliJ que deve conter uma classe chamada Perfil, com o método main, que ao executar deverá imprimir um cumprimento personalizado no console.
+    Por exemplo, "Olá, [Seu Nome]!".
+- Modifique o programa para imprimir uma segunda mensagem de cumprimento personalizada. Por exemplo, "Tudo bem?".
+*/
+
+public class Perfil {
+
+    public static void main(String[] args) {
+        String nome = "João Pedro";
+
+        System.out.println("Olá, " + nome + "!");
+        System.out.println("Tudo bem?");
+    }
+
+}
